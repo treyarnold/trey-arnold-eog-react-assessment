6718720d3eb10cfde5cf227b06de1523f84105a6
Trey Arnold <arnold.trey80@gmail.com>
2019-07-26 09:09:44 -0500

    added cards to display most recent readings

--- src/components/Dashboard/GraphMetrics/GraphMetrics.js
+++ src/components/Dashboard/GraphMetrics/GraphMetrics.js
@@ -9,6 +9,8 @@ import {
   Tooltip
 } from "recharts";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { CardContent, Card, Grid, Typography } from "@material-ui/core";
 import { useQuery } from "urql";
 import gql from "graphql-tag";
 
@@ -33,6 +35,18 @@ const getHeartbeat = gql`
   }
 `;
 
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingLeft: "10%",
+    marginBottom: 40,
+    maxWidth: "95%"
+  },
+  card: {
+    display: "flex",
+    flexDirection: "Column"
+  }
+}));
+
 const getMetrics = state => {
   const { measurements, selectedMetrics } = state.metrics;
   return {
@@ -44,12 +58,14 @@ const getMetrics = state => {
 // eslint-disable-next-line react/prop-types
 const GraphMetrics = ({ lastMetricSelected }) => {
   const [heartBeat, setHeartBeat] = useState();
+  const [lastMeasure, setLastMeasure] = useState({});
   const dispatch = useDispatch();
   const { measurements, selectedMetrics } = useSelector(getMetrics);
   const [chartData, setChartData] = useState();
   const heartBeats = useQuery({
     query: getHeartbeat
   })[0].data;
+  const classes = useStyles();
 
   useEffect(() => {
     if (heartBeats) {
@@ -80,6 +96,7 @@ const GraphMetrics = ({ lastMetricSelected }) => {
 
   useEffect(() => {
     const selected = measurements[selectedMetrics[0]];
+    const newLastMeasure = {};
     if (selected && selected.points.length) {
       const newChartData = [];
       const arraySize = selected.points.length;
@@ -94,26 +111,46 @@ const GraphMetrics = ({ lastMetricSelected }) => {
         });
         newChartData.push(dataPoint);
       }
+      selectedMetrics.map(metric => {
+        newLastMeasure[metric] =
+          measurements[metric].points[
+            measurements[metric].points.length - 1
+          ].value;
+        return null;
+      });
+      setLastMeasure(newLastMeasure);
       setChartData(newChartData);
     }
   }, [measurements]);
 
   return selectedMetrics.length ? (
-    <ResponsiveContainer width="80%" height="60%" margin={{ left: 50 }}>
-      <LineChart data={chartData}>
+    <React.Fragment>
+      <Grid container className={classes.root} spacing={2}>
         {selectedMetrics.map(metric => (
-          <Line
-            key={Math.random()}
-            dot={false}
-            type="monotone"
-            dataKey={metric}
-          />
+          <Card key={Math.random()} className={classes.card}>
+            <CardContent>
+              <Typography>{metric}</Typography>
+              <Typography>{lastMeasure[metric]}</Typography>
+            </CardContent>
+          </Card>
         ))}
-        <XAxis dataKey="at" />
-        <YAxis label={{ value: "PSI", angle: -90, position: "insideLeft" }} />
-        <Tooltip />
-      </LineChart>
-    </ResponsiveContainer>
+      </Grid>
+      <ResponsiveContainer width="80%" height="60%" margin={{ left: 50 }}>
+        <LineChart data={chartData}>
+          {selectedMetrics.map(metric => (
+            <Line
+              key={Math.random()}
+              dot={false}
+              type="monotone"
+              dataKey={metric}
+            />
+          ))}
+          <XAxis dataKey="at" />
+          <YAxis label={{ value: "PSI", angle: -90, position: "insideLeft" }} />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </React.Fragment>
   ) : null;
 };
 
